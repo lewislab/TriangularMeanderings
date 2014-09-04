6f159a1384597ed86562c9120ece1051300bc25d
Steve Kelly <kd2cca@gmail.com>
2014-09-04 10:53:02 -0400

    add extrusion width and walls

--- new_meander.py
+++ new_meander.py
@@ -7,27 +7,50 @@ g = G()
 
 box_x = 10
 box_y = 10
-spacing = 1
-meander_start = 'LR'
+walls = 3
+spacing = 5
+meander_start = 'LL'
+extrusion_width = 0.3
 
-def triangular_meander(g, x, y, spacing, start='LR'):
-    # Calculate meander
-    spacing = g.meander_spacing(box_y,spacing)
-    passes = g.meander_passes(box_y, spacing)
+start_height = 0
+layer_count = 10
+layer_thick = 0.2
 
+def tri_meander_length(g, x, y, spacing, extrusion_width):
     # Calculate tri wave
-    tri_x_half_cycle = spacing/math.sqrt(3)
-    tri_y_half_cycle = spacing
-
-    cycles = box_x/(tri_x_half_cycle*2)
+    tri_x_length = x-extrusion_width
+    tri_y_half_cycle = g.meander_spacing(y,spacing)-extrusion_width*2
+    tri_x_half_cycle = (tri_y_half_cycle/math.sqrt(3))
+    cycles = tri_x_length/(tri_x_half_cycle*2)
 
     if round(cycles) <= cycles: # if we round down
         cycles = round(cycles) + 0.5
     elif round(cycles) >= cycles: # if we round up
         cycles = round(cycles) - 0.5
 
+    return cycles*tri_x_half_cycle*2+extrusion_width*2
+
+
+def triangular_meander(g, x, y, spacing, start='LR', extrusion_width=0.0):
+    # Calculate meander
+    spacing = g.meander_spacing(y,spacing)
+    passes = g.meander_passes(y, spacing)
+
+    # Calculate tri wave
+    tri_x_length = x-extrusion_width
+    tri_y_half_cycle = spacing-extrusion_width*2
+    tri_x_half_cycle = (tri_y_half_cycle/math.sqrt(3))
+
+    cycles = tri_x_length/(tri_x_half_cycle*2)
+
+    if round(cycles) <= cycles: # if we round down
+        cycles = round(cycles) + 0.5
+    elif round(cycles) >= cycles: # if we round up
+        cycles = round(cycles) - 0.5
+
+    new_box_x = cycles*tri_x_half_cycle*2+extrusion_width*2
     # Do meander
-    g.meander(cycles*tri_x_half_cycle*2, box_y, spacing, start=meander_start)
+    g.meander(new_box_x, y, spacing, start=meander_start)
 
     tri_wave_start = meander_start
     if passes%2==1.0:
@@ -36,23 +59,45 @@ def triangular_meander(g, x, y, spacing, start='LR'):
         elif meander_start[1] == 'L':
             tri_wave_start = tri_wave_start[0]+'R'
 
+    g.relative() #TODO: Return to origin axis mode
+    if tri_wave_start == 'LL':
+        g.move(x=-extrusion_width, y=-extrusion_width)
+    elif tri_wave_start == 'LR':
+        g.move(x=extrusion_width, y=-extrusion_width)
+    elif tri_wave_start == 'UL':
+        g.move(x=-extrusion_width, y=extrusion_width)
+    elif tri_wave_start == 'UR':
+        g.move(x=extrusion_width, y=extrusion_width)
+
     for i in range(int(passes)):
         g.triangular_wave(tri_x_half_cycle, tri_y_half_cycle, cycles, start=tri_wave_start)
+
+        # cross over meander
+        if tri_wave_start[0] == 'U':
+            g.move(x=0, y=extrusion_width*2)
+        elif tri_wave_start[0] == 'L':
+            g.move(x=0, y=-extrusion_width*2)
+
+        # alternate direction
         if tri_wave_start[1] == 'R':
             tri_wave_start = tri_wave_start[0]+'L'
         elif tri_wave_start[1] == 'L':
             tri_wave_start = tri_wave_start[0]+'R'
 
 
-start_height = 0
-layer_count = 10
-layer_thick = 0.2
-
 heights = np.arange(start_height, layer_count*layer_thick, layer_thick)
-g.absolute()
+
+tri_length = tri_meander_length(g, box_x, box_y, spacing, extrusion_width)
 for height in heights:
-    g.move(x=0, y=0, z=height)
-    triangular_meander(g, box_x, box_y, spacing, start=meander_start)
+    for i in range(walls, 0, -1):
+        g.absolute()
+        g.move(x=-extrusion_width*i, y=-extrusion_width*i, z=height)
+        g.relative()
+        g.rect(tri_length+extrusion_width*i*2, box_y+extrusion_width*i*2)
+
+    g.absolute()
+    g.move(0,0)
+    triangular_meander(g, box_x, box_y, spacing, start=meander_start, extrusion_width=extrusion_width)
 
 
 
